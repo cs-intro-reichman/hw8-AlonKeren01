75e98000c4f8a63ce9f55f454ae46ce660182f76
AlonKeren01 <alon.keren01@post.runi.ac.il>
2025-01-06 17:34:38 +0200

    try2

--- User.java
+++ User.java
@@ -75,7 +75,7 @@
     /** Removes the given name from the follows list of this user. If successful, returns true.
      *  If the name is not in the list, does nothing and returns false. */
     public boolean removeFollowee(String name) {
-        if(name == null || follows(name)){
+        if(name == null || !follows(name)){
             return false;
         }
         //// Replace the following statement with your code
